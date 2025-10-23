90d977d13517683b2ac8e4c6f2695c0f18850af4
root <root@srv1005155.hstgr.cloud>
2025-10-23 19:09:22 +0000

    end

--- frontend/src/pages/home/ctaSection/CTASection.jsx
+++ frontend/src/pages/home/ctaSection/CTASection.jsx
@@ -15,11 +15,8 @@ export default function CTASection() {
         نحن في  وسيط آسيا نلتزم بتقديم أفضل خدمات الاستقدام بسرعة وأمان لتلبية احتياجاتك.
         <br /> اختَر العامل المناسب لك بخطوات بسيطة وسهلة.
       </p>
-<<<<<<< HEAD
-      <Button variant="contained" color="primary" size="hero" link="https://wa.me/+966555872001" anchor>
-=======
+      <Button variant="contained" color="primary" size="hero" link="https://wa.me/"00966555872001" anchor>
       <Button variant="contained" color="primary" size="hero" link="tel:00966555872001" anchor>
->>>>>>> 7e696bd02a3f4c1ec89f3676a6a31094aa2cc82e
         تواصل معنا
         <BsWhatsapp />
       </Button>
